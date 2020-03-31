4a3b295a2f53881416cf2080e0036aa0e55e4bd9
DeepakVelmurugan <55920093+DeepakVelmurugan@users.noreply.github.com>
2020-03-31 17:28:51 +0530

    Delete computeCost.m

--- computeCost.m
+++
@@ -1,8 +0,0 @@
-function J = computeCost(X, y, theta)
-% Initialize some useful values
-m = length(y); % number of training examples 
-J = 0;
-h = X * theta; % hypothesis function (Theta' * x = theta_0 + theta_1 * x_1)
-squaredErrors = (h - y) .^ 2;
-J = (1 / (2 * m)) * sum(squaredErrors);
-end
